1a9e6039e1f42d69684135d7e574f8f1fd8934b5
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-05-06 12:51:00 +0700

    Update Animation Landing :skull:

--- landing/script.js
+++ landing/script.js
@@ -1,5 +1,24 @@
-let getAllFullPage = document.querySelectorAll('.fullpage');
+let getAllFullPage = document.querySelectorAll(".event-triger");
+let rocket = document.querySelector(".rocket");
+let test = new IntersectionObserver(
+  function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        rocket.dataset.status = entry.target.classList[0];
+        console.log(entry.target.classList[0]);
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
 
-getAllFullPage.forEach(ele => {
-    ele.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-})
+// For Dev Only - To see Section Intersection 📐
+// getAllFullPage.forEach((element) => {
+//     element.style.backgroundColor = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+// });
+
+getAllFullPage.forEach((el) => {
+  test.observe(el);
+});
