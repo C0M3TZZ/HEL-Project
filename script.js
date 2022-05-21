0636e02d9a1059d7999a39175739cc8e8ba3c1ad
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-05-21 17:12:05 +0700

    Try to update to recent git

--- script.js
+++ script.js
@@ -23,14 +23,13 @@ getAllFullPage.forEach((el) => {
   test.observe(el);
 });
 
+let stars = document.getElementById("stars");
+let sun = document.getElementById("sun");
+let header = document.querySelector("header");
 
-    let stars = document.getElementById('stars');
-    let sun = document.getElementById('sun');
-    let header = document.querySelector('header');
-
-    window.addEventListener('scroll', function(){
-        let value = window.scrollY;
-        stars.style.left = value * 0.15 + 'px';
-        sun.style.left = value * -0.2 + 'px';
-        header.style.top = value * 0.5 + 'px';
-    })
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  stars.style.left = value * 0.15 + "px";
+  sun.style.left = value * -0.2 + "px";
+  header.style.top = value * 0.5 + "px";
+});
